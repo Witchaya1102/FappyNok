525923d156fe6f5fb33ad103d6874d28ed49bb8f
N4TSU <77868849+Noraphat-thod@users.noreply.github.com>
2021-10-15 01:28:32 +0700

    Update
    Unity = เพิ่ม โฟร์เดอร์  ใส่ ผี (สิ่งกีดขวาง) Design = ลบทดลอง เพิ่มดีไซผี

---
+++ FappyNok/Assets/Scripts/Manager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
