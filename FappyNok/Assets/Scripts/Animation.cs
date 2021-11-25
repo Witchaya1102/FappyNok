04172def165169e81b6e139c8fed09ed28adc0da
N4TSU <94550800+Noraphat-natsu@users.noreply.github.com>
2021-11-25 22:52:08 +0700

    1
    1

---
+++ FappyNok/Assets/Scripts/Animation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            anim.SetInteger("Anima", 1);
+        }
+        
+        if (Input.GetKeyUp("space"))
+        {
+            anim.SetInteger("Anima", 0);
+        }
+    }
+}
