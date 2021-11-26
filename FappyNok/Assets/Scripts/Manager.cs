8b3714f950500801733dd75a6a4e6d8e0500fce4
N4TSU <94550800+Noraphat-natsu@users.noreply.github.com>
2021-11-26 15:34:06 +0700

    update
    เพิ่ม ซีนไหม่    /    แก้ไข้ หน้าแรก เปรียน ซาว

--- FappyNok/Assets/Scripts/Manager.cs
+++ FappyNok/Assets/Scripts/Manager.cs
@@ -6,7 +6,19 @@ public class Manager : MonoBehaviour
     public void Play()
     {
 
+        SceneManager.LoadScene("Advice");
+    }
+    
+    public void Play_game()
+    {
+
         SceneManager.LoadScene("Game");
+    }
+    
+    public void Back()
+    {
+
+        SceneManager.LoadScene("HomePage");
     }
     
     public void ExitGame()
